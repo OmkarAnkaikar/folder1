168a75a81f5014120560d6036053b237e9ca668d
Ubuntu <ubuntu@ip-172-31-41-123.ap-south-1.compute.internal>
2025-03-14 12:24:32 +0000

    My first book folders

---
+++ file.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#comments
+
+
+echo "This is line on sh"
+echo "This is 2nd line"
+
+name="omkar"
+
+echo "my name is $name"
+
+read -p "Enter your toppen nav : " Enter_name
+
+echo my toppen name is $Enter_name
